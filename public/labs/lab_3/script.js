b9fac446054ec9a8d179319e96a974399ea15f4f
daitch-umd <78330551+daitch-umd@users.noreply.github.com>
2021-02-12 22:54:02 -0500

    lab 3

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,32 @@
 /* Put your javascript in here */
+
+const images = carousel.querySelectorAll('li');
+images.forEach((element) => {
+    element.style.position = 'relative';
+    element.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${element}</span>`);
+});
+
+/* configuration */
+let width = 130; // image width
+let count = 3; // visible images count
+
+let list = carousel.querySelector('ul');
+let listElems = carousel.querySelectorAll('li');
+
+let position = 0; // ribbon scroll position
+
+carousel.querySelector('.prev').onclick = function() {
+  // shift left
+  position += width * count;
+  // can't move to the left too much, end of images
+  position = Math.min(position, 0)
+  list.style.marginLeft = position + 'px';
+};
+
+carousel.querySelector('.next').onclick = function() {
+  // shift right
+  position -= width * count;
+  // can only shift the ribbbon for (total ribbon length - visible count) images
+  position = Math.max(position, -width * (listElems.length - count));
+  list.style.marginLeft = position + 'px';
+};
