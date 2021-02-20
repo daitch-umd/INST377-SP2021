b78d1d918566c7b34932e9a32d857a24e516dc31
daitch-umd <78330551+daitch-umd@users.noreply.github.com>
2021-02-19 21:49:46 -0500

    lab 4

--- server.js
+++ server.js
@@ -44,6 +44,7 @@ app.route('/api')
     console.log('Form data in res.body', req.body);
     console.log('Now send something back to your client');
     // res.json({data: dataToSendToFrontEnd});
+    res.send('Hello World');
   });
 
 app.listen(port, async () => {
