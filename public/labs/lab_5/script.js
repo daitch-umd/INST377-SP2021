910ca76b3c60a00eba0125abb95976919a903b39
daitch-umd <78330551+daitch-umd@users.noreply.github.com>
2021-03-12 23:44:59 -0500

    Search not returning, but marker code in place.

--- public/labs/lab_5/script.js
+++ public/labs/lab_5/script.js
@@ -1,67 +1,3 @@
-/*
-async function windowActions() {
-  const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
-
-  const request = await fetch(endpoint);
-  const restaurants = await request.json();
-
-  function findMatches(wordToMatch, restaurants) {
-    const tempArr = restaurants.filter(place => {
-      const regex = new RegExp(wordToMatch, 'gi');
-      return place.city.match(regex) || place.name.match(regex) || place.category.match(regex)
-    });
-    const tempSet = new Set(tempArr.map(place => place.establishment_id));
-    const unique_ids = Array.from(tempSet).slice(0,9);
-    console.log(unique_ids);
-    let id_length  = unique_ids.length-1;
-    const results = [];
-    //iterate through the list
-    while (id_length >= 0) {
-      for (place in restaurants) {
-        // compare the value of the restaurants key to value in unique keys
-        if (restaurants[place].establishment_id == unique_ids[id_length] && unique_ids.length > 0) {
-          
-          let catThis = restaurants[place];
-          results.push(catThis);
-          console.log(unique_ids.pop());
-          id_length = unique_ids.length-1;
-        }
-      }
-    }
-    
-    return results;
-  }
-
-function displayMatches(event) {
-  const matchArray = findMatches(event.target.value, restaurants);
-  const html = matchArray.map(place => {
-    return `
-    <div class="box">
-      <li>
-      <address>
-        <h2 class="subtitle is-3"><span class="name">${place.name}</span></h2>
-        <span class="address">${place.address_line_1}</span> <br>
-        ${place.city}, ${place.state} ${place.zip} <br>
-        </br>
-        <span class="category">${place.category} <br>
-        <span class="quarantining">Quarantining: ${place.ill_workers_restricted}</span>
-      </address>
-      </li>
-    </div>
-    `
-  }).join('');
-  suggestions.innerHTML= html;
-}
-
-  const searchInput  = document.querySelector('.input');
-  const suggestions = document.querySelector('.suggestions');
-
-  searchInput.addEventListener('keyup', (evt) => { displayMatches(evt) });
-
-}
-*/
-window.onload = windowActions();
-
 function mapInit() {
   let mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
@@ -74,7 +10,7 @@ function mapInit() {
     accessToken: 'pk.eyJ1IjoiZGFpdGNoLXVtZCIsImEiOiJja203Mms0bGMwdHppMm5wbWJ3ZG9sbTl3In0.aKCQkdfv-CtDjm7euMwsxQ'
   }).addTo(mymap);
   // follow the Leaflet Getting Started tutorial here
-  return map;
+  return mymap;
 }
 
 async function dataHandler(mapObjectFromFunction) {
@@ -88,7 +24,7 @@ async function dataHandler(mapObjectFromFunction) {
   function findMatches(wordToMatch, restaurants) {
     const tempArr = restaurants.filter(place => {
       const regex = new RegExp(wordToMatch, 'gi');
-      return place.city.match(regex) || place.name.match(regex) || place.category.match(regex)
+      return place.zip.match(regex)
     });
     const tempSet = new Set(tempArr.map(place => place.establishment_id));
     const unique_ids = Array.from(tempSet).slice(0,9);
@@ -108,36 +44,57 @@ async function dataHandler(mapObjectFromFunction) {
         }
       }
     }
+  
+    const filteredData = results.filter((record) => record.geocoded_column_1);
     
     return results;
   }
 
-function displayMatches(event) {
-  const matchArray = findMatches(event.target.value, restaurants);
-  const html = matchArray.map(place => {
-    return `
-    <div class="box">
-      <li>
-      <address>
-        <h2 class="subtitle is-3"><span class="name">${place.name}</span></h2>
-        <span class="address">${place.address_line_1}</span> <br>
-        ${place.city}, ${place.state} ${place.zip} <br>
-        </br>
-        <span class="category">${place.category} <br>
-        <span class="quarantining">Quarantining: ${place.ill_workers_restricted}</span>
-      </address>
-      </li>
-    </div>
-    `
-  }).join('');
-  suggestions.innerHTML= html;
-}
 
-  const searchInput  = document.querySelector('.input');
+  function displayMatches(event) {
+    const matchArray = findMatches(event.target.value, restaurants);
+    const html = matchArray.map(place => {
+    return `
+      <div class="box">
+        <li>
+          <address>
+            <h2 class="subtitle is-3"><span class="name">${place.name}</span></h2>
+            <span class="address">${place.address_line_1}</span> <br>
+            ${place.city}, ${place.state} ${place.zip} <br>
+            </br>
+            <span class="category">${place.category} <br>
+            <span class="quarantining">Quarantining: ${place.ill_workers_restricted}</span>
+          </address>
+        </li>
+      </div>
+    `
+    }).join('');
+    suggestions.innerHTML= html;
+  }
+
+  const searchInput = document.querySelector('.input');
   const suggestions = document.querySelector('.suggestions');
 
-  searchInput.addEventListener('keyup', (evt) => { displayMatches(evt) });
+  //searchInput.addEventListener('keyup', (evt) => { displayMatches(evt) });
+  
+  searchInput.addEventListener('submit', async (myEvent) => { 
+    //displayMatches(myEvent);
+    
+    myEvent.preventDefault();
+    const filtered = findMatches(myEvent.target.value, restaurants).filter((record) => record.geocoded_column_1);
 
+    filtered.forEach((item) => {
+      const longLat = item.geocoded_column_1.coordinates;
+      const marker = L.marker([longLat[1], longLat[0]]).addTo(mapObjectFromFunction);
+      const appendItem = document.createElement('li');
+      appendItem.classList.add('block');
+      appendItem.classList.add('list-item');
+      appendItem.innerHTML = `<div class="list-header is-size-5">${item.name}</div><address class="is-size-6">${item.address_line_1}</address>`;
+      suggestions.append(appendItem);
+    });
+    
+  });
+  
 }
 
 async function windowActions() {
